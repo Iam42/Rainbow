86282c1f4405c821f392f62438e50c54785d4fd1
泛槎 <zhanggan.zg@alibaba-inc.com>
2015-08-30 12:45:54 +0800

    搭建工程结构

---
+++ rainbowlib/src/main/java/liam42/com/rainbowlib/api/Rainbow.java
@@ -0,0 +1,13 @@
+package liam42.com.rainbowlib.api;
+
+import android.util.Log;
+
+/**
+ * Created by a42 on 15/8/30.
+ */
+public class Rainbow {
+
+    public static void startWebPage() {
+        //todo
+    }
+}
